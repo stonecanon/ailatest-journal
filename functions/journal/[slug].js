1d90769f055760f7895a1b1011fddd9e1c87c244
zhizhi <43259074+stonecanon@users.noreply.github.com>
2026-05-31 06:41:52 +0800

    cleanup: remove stale function files

--- functions/journal/[slug].js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(ctx) {
-  return new Response('slug=' + JSON.stringify(ctx.params.slug) + ' url=' + ctx.request.url, {
-    headers: { 'Content-Type': 'text/plain' }
-  });
-}
